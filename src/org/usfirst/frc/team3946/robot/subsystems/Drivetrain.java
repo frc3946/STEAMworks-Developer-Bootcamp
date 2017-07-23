3b24430a3ac5a8a3c0e1b1befd974f9a44513434
Gustave <gixxygamma@gmail.com>
2017-07-22 20:12:45 -0500

    Removed unneeded imports

--- src/org/usfirst/frc/team3946/robot/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team3946/robot/subsystems/Drivetrain.java
@@ -1,6 +1,5 @@
 package org.usfirst.frc.team3946.robot.subsystems;
 
-import org.usfirst.frc.team3946.robot.Robot;
 import org.usfirst.frc.team3946.robot.RobotMap;
 import org.usfirst.frc.team3946.robot.commands.JoystickTankDrive;
 
